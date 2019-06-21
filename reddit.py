c6e824b6fd4e2bfbfd7a99f6d14ef1f8bcf8ddb6
Brian Choi <sukhoonchoi1@gmail.com>
2019-06-21 20:48:44 +1200

    add world news filter

--- reddit.py
+++ reddit.py
@@ -4,21 +4,16 @@ import prawcore
 try: 
     reddit = praw.Reddit('bot1', user_agent='cpqp user agent')
 
-    print(reddit.read_only)
+    subreddit = reddit.subreddit('WorldNews')
+                            
+    hot_submissions = subreddit.hot(limit=10)
     
-    subreddit = reddit.subreddit('Python')
-
-    # hot_python = subreddit.hot(limit=15)
-
-    # for submission in hot_python: 
-    #     if not submission.stickied:
-    #         print(submission.title)
-    #         print(submission.score)
-    #         print(submission.id)
-    #         print(submission.url)
-    #         print('\n\n')
+    for submission in hot_submissions: 
+        if ("trump" in submission.title.lower()):
+            print(submission.title)
+            print(submission.score)
+            print("\n\n")
             
 except prawcore.exceptions.ResponseException as e:
     print e
-    
     
